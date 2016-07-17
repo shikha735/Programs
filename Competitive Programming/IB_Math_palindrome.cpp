6c18485d6355d7466dabaacb20cf9b6f1e887e46
Shikha Sharma <shikha735@gmail.com>
2016-07-17 22:36:48 +0530

    Palindrome number - Another method

--- Competitive Programming/IB_Math_palindrome.cpp
+++ Competitive Programming/IB_Math_palindrome.cpp
@@ -1,6 +1,20 @@
 // Determine whether an integer is a palindrome. Do this without extra space.
 // https://www.interviewbit.com/courses/programming/topics/math/problems/palindrome/
 
+// Method 2
+/*bool isPalindrome(int N){
+    int div=1;
+    while(N/div >= 10)
+        div *= 10;
+    while(N!=0){
+        if(N/div != N%10)
+            return false;
+        N = (N%div)/10;
+        div /= 100;
+    }
+    return true;
+}*/
+
 bool Solution::isPalindrome(int A) {
     int rem = 0, res = 0, N = A;
     bool flag= false;
